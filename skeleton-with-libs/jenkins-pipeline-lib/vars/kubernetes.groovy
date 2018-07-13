3179e61d63c1a5c826e7ad63908fe25fad2621a0
alacambra <alacambra@gmail.com>
2018-07-13 14:06:27 +0200

    package renamed

--- skeleton-with-libs/jenkins-pipeline-lib/vars/kubernetes.groovy
+++
@@ -1,7 +0,0 @@
-def apply(String file, String namespace) {
-    sh "kubectl apply --namespace=$namespace -f $file"
-}
-
-def rolloutStatus(String deployment, String namespace) {
-    sh "kubectl rollout status --namespace=$namespace deployment $deployment"
-}
