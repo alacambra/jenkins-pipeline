93bd743de6f291b8d5baa7bd3358720a2254c0d8
alacambra <alacambra@gmail.com>
2018-07-16 19:27:09 +0200

    added jenkinsfile

---
+++ sample-app/hello-app/src/test/java/tech/lacambra/HelloResourceTest.java
@@ -0,0 +1,21 @@
+package tech.lacambra;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class HelloResourceTest {
+
+    HelloResource cut;
+
+    @Before
+    public void setUp() throws Exception {
+        cut = new HelloResource();
+    }
+
+    @Test
+    public void sayHello() {
+        assertEquals(200, cut.sayHello().getStatus());
+    }
+}
