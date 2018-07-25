11d48050f3c8cb656c22a1ab2b027d0e7da27850
alacambra <alacambra@gmail.com>
2018-07-25 21:05:18 +0200

    try using UT

--- sample-app/hello-st/src/test/java/tech/lacambra/HelloRemoteTest.java
+++ sample-app/hello-st/src/test/java/tech/lacambra/HelloRemoteTest.java
@@ -6,13 +6,14 @@ import org.junit.Test;
 
 import javax.ws.rs.core.Response;
 
-public class HelloRemoteIT {
+public class HelloRemoteTest {
 
     @Rule
     public HelloClient helloClient = new HelloClient();
 
     @Test
     public void sayRemoteHello() {
+        System.out.println("Running test");
         Response response = helloClient.sayHello();
         Assert.assertEquals(200, response.getStatus());
     }
