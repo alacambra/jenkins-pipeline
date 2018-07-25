bd9794dc67e57928443573e65e817cbaeb749b27
alacambra <alacambra@gmail.com>
2018-07-25 20:38:36 +0200

    force wait time

--- sample-app/hello-st/src/test/java/tech/lacambra/HelloClient.java
+++ sample-app/hello-st/src/test/java/tech/lacambra/HelloClient.java
@@ -13,7 +13,7 @@ import java.util.regex.Pattern;
 public class HelloClient extends ExternalResource {
 
     private static final int STARTUP_TIMEOUT = 30;
-    private static final int STARTUP_PING_DELAY = 2;
+    private static final int STARTUP_PING_DELAY = 30;
 
     private String helloUri = "http://localhost:8080/app/resources/hello";
     private Pattern helloUriPattern = Pattern.compile(helloUri + "/[a-z0-9\\-]+");
@@ -32,12 +32,13 @@ public class HelloClient extends ExternalResource {
     }
 
     private void waitForApplicationStartUp() {
-        final long timeout = System.currentTimeMillis() + STARTUP_TIMEOUT * 1000;
-        while (helloTarget.request().head().getStatusInfo().getFamily() != Response.Status.Family.SUCCESSFUL) {
-            System.out.println("waiting for application startup ...");
-            LockSupport.parkNanos(1_000_000_000 * STARTUP_PING_DELAY);
-            if (System.currentTimeMillis() > timeout)
-                throw new AssertionError("Application wasn't started before timeout!");
-        }
+        LockSupport.parkNanos(1_000_000_000 * STARTUP_PING_DELAY);
+//        final long timeout = System.currentTimeMillis() + STARTUP_TIMEOUT * 1000;
+//        while (helloTarget.request().head().getStatusInfo().getFamily() != Response.Status.Family.SUCCESSFUL) {
+//            System.out.println("waiting for application startup ...");
+//            LockSupport.parkNanos(1_000_000_000 * STARTUP_PING_DELAY);
+//            if (System.currentTimeMillis() > timeout)
+//                throw new AssertionError("Application wasn't started before timeout!");
+//        }
     }
 }
